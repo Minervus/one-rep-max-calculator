e88c63678096bc3f88f05a5b4714441b14a4fb4f
Minervus <tnguyen411@gmail.com>
2023-09-28 15:11:45 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "One Rep Max Calculator"
+include(":app")
+ 
